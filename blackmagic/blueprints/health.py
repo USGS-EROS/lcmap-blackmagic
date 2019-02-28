de580c7da1349c0ec21e393c8a95391f7297565d
David Hill <davehill75@gmail.com>
2019-02-28 10:51:56 -0600

    clean up blueprints and parallel originating from failed merge

--- blackmagic/blueprints/health.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import jsonify
-
-health = Blueprint('health', __name__)
-
-@health.route('/health', methods=['GET'])
-def health_fn():
-    return jsonify(True)
