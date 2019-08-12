cab92185e271b09a7fd253e98ac9820679515574
David Hill <dhill@contractor.usgs.gov>
2019-08-12 14:22:55 -0500

    separating lines in prediction to help pinpoint error.

--- blackmagic/blueprints/prediction.py
+++ blackmagic/blueprints/prediction.py
@@ -149,9 +149,10 @@ def load_data(ctx, cfg):
 @skip_on_exception
 @measure
 def load_model(ctx):
+
+    t = _ceph.select_tile(ctx['tx'], ctx['ty'])
     
-    fn = excepts(StopIteration,
-                 lambda _: bytes.fromhex(first(_ceph.select_tile(ctx['tx'], ctx['ty']))['model']))
+    fn = excepts(StopIteration, lambda _: bytes.fromhex(first(t)['model']))
 
     model = fn(None)
 
