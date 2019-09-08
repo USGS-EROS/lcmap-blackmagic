e0713ad8623d2d40ba02e08f7ad75eaabbe655ba
David Hill <dhill@contractor.usgs.gov>
2019-09-08 18:35:19 -0500

    remove instrumentation, turn down gunicorn logging to warning

--- bin/blackmagic.sh
+++ bin/blackmagic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-gunicorn --max-requests $MAX_REQUESTS --timeout $WORKER_TIMEOUT --bind :$HTTP_PORT --workers $WORKERS blackmagic.app:app
+gunicorn --log-level warning --max-requests $MAX_REQUESTS --timeout $WORKER_TIMEOUT --bind :$HTTP_PORT --workers $WORKERS blackmagic.app:app
