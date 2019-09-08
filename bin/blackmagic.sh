8bef718ff9d01604a754edc3fae20017a456fea3
David Hill <dhill@contractor.usgs.gov>
2019-09-08 15:13:11 -0500

    correct --max-requests paramter

--- bin/blackmagic.sh
+++ bin/blackmagic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-gunicorn --max_requests $max_requests --timeout $WORKER_TIMEOUT --bind :$HTTP_PORT --workers $WORKERS blackmagic.app:app
+gunicorn --max-requests $MAX_REQUESTS --timeout $WORKER_TIMEOUT --bind :$HTTP_PORT --workers $WORKERS blackmagic.app:app
